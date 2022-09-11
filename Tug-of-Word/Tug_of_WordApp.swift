f9508040478fd24608d14b4162e3a1d5b1c3aa9b
Alec <robert.hovey95@gmail.com>
2022-09-11 12:36:30 -0400

    Initial Commit

---
+++ Tug-of-Word/Tug_of_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tug_of_WordApp.swift
+//  Tug-of-Word
+//
+//  Created by Robert Alec Hovey on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Tug_of_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
