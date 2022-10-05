c92f7f1edf5431b59780989d4647ca2aadbf320d
Robert Hovey <robert.hovey95@gmail.com>
2022-10-05 12:18:10 -0400

    fixed up some navigation renamed files

--- Tug-of-Word/AllGamesView.swift
+++ Tug-of-Word/AllGamesView.swift
@@ -1,5 +1,5 @@
 //
-//  NewGameView.swift
+//  AllGamesView.swift
 //  Tug-of-Word
 //
 //  Created by Robert Alec Hovey on 10/5/22.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct NewGameView: View {
+struct AllGamesView: View {
     var body: some View {
         Text("New Game")
     }
 }
 
-struct NewGameView_Previews: PreviewProvider {
+struct AllGamesView_Previews: PreviewProvider {
     static var previews: some View {
-        NewGameView()
+        AllGamesView()
     }
 }
--- Tug-of-Word/StartScreenView.swift
+++ Tug-of-Word/StartScreenView.swift
@@ -1,5 +1,5 @@
 //
-//  StartScreen.swift
+//  StartScreenView.swift
 //  Tug-of-Word
 //
 //  Created by Robert Alec Hovey on 9/11/22.
@@ -7,27 +7,30 @@
 
 import SwiftUI
 
-struct StartScreen: View {
+struct StartScreenView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("Tug-of-Word").padding()
+                Spacer()
+                Text("Tug-of-Word").font(.title).padding()
+                Text("Beta Version 1.0.0")
                 Spacer()
                 VStack {
-                    NavigationLink(destination: NewGameView()) {
+                    NavigationLink(destination: AllGamesView()) {
                         Text("Play").frame(maxWidth: .infinity).padding(.all, 10.0)
                     }.buttonStyle(.borderedProminent)
                     NavigationLink(destination: SettingsView()) {
                         Text("Settings").frame(maxWidth: .infinity).padding(.all, 10.0)
                     }.buttonStyle(.borderedProminent)
                 }.padding()
+                Spacer()
             }.padding()
-        }.navigationTitle("Games")
+        }
     }
 }
 
-struct StartScreen_Previews: PreviewProvider {
+struct StartScreenView_Previews: PreviewProvider {
     static var previews: some View {
-        StartScreen()
+        StartScreenView()
     }
 }
