a1877ef69dd6bc1e318001786bad43ab38264800
Robert Hovey <robert.hovey95@gmail.com>
2022-10-05 10:38:17 -0400

    added NewGameView added SettingsView

---
+++ Tug-of-Word/NewGameView.swift
@@ -0,0 +1,20 @@
+//
+//  NewGameView.swift
+//  Tug-of-Word
+//
+//  Created by Robert Alec Hovey on 10/5/22.
+//
+
+import SwiftUI
+
+struct NewGameView: View {
+    var body: some View {
+        Text("New Game")
+    }
+}
+
+struct NewGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewGameView()
+    }
+}
