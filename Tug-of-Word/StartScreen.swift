036c6d28e5ec44aa397325dcbb326a0a686a08cd
Robert Hovey <robert.hovey95@gmail.com>
2022-10-05 11:50:54 -0400

    completed navigation changed buttons to navigation links

--- Tug-of-Word/StartScreen.swift
+++ Tug-of-Word/StartScreen.swift
@@ -9,28 +9,20 @@ import SwiftUI
 
 struct StartScreen: View {
     var body: some View {
-        VStack {
-            Text("Tug-of-Word").padding()
-            Spacer()
+        NavigationView {
             VStack {
-                Button(
-                    action: {
-                        print("Play")
-                    },
-                    label: {
+                Text("Tug-of-Word").padding()
+                Spacer()
+                VStack {
+                    NavigationLink(destination: NewGameView()) {
                         Text("Play").frame(maxWidth: .infinity).padding(.all, 10.0)
-                    }
-                ).buttonStyle(.borderedProminent)
-                Button(
-                    action: {
-                        print("Settings")
-                    },
-                    label: {
+                    }.buttonStyle(.borderedProminent)
+                    NavigationLink(destination: SettingsView()) {
                         Text("Settings").frame(maxWidth: .infinity).padding(.all, 10.0)
-                    }
-                ).buttonStyle(.borderedProminent)
+                    }.buttonStyle(.borderedProminent)
+                }.padding()
             }.padding()
-        }.padding()
+        }.navigationTitle("Games")
     }
 }
 
